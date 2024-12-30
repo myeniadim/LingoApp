c44624db869fc0b0fd1dda059a9bc231b5a71bb0
myeniadim <m.yeniadimm@gmail.com>
2024-12-30 19:23:26 +0300

    implement bottom navigation bar

--- app/src/main/java/com/msku/ceng3505/lingoapp/MainActivity.java
+++ app/src/main/java/com/msku/ceng3505/lingoapp/MainActivity.java
@@ -2,23 +2,46 @@ package com.msku.ceng3505.lingoapp;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
+
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.msku.ceng3505.lingoapp.databinding.ActivityMainBinding;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new SectionsFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            if (item.getItemId() == R.id.sections) {
+                replaceFragment(new SectionsFragment());
+            }else if (item.getItemId() == R.id.flashcards) {
+                replaceFragment(new FlashcardsFragment());
+            } else if (item.getItemId() == R.id.profile) {
+                replaceFragment(new ProfileFragment());
+            }
+            return true;
         });
+
+
+    }
+
+    private void replaceFragment(Fragment selectedFragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, selectedFragment);
+        fragmentTransaction.commit();
     }
 }
