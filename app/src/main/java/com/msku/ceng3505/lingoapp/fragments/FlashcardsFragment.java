2ff6dcf7a16cc0a16cd8e0e4a0139ca15d90e577
myeniadim <m.yeniadimm@gmail.com>
2025-01-10 20:57:31 +0300

    small changes

--- app/src/main/java/com/msku/ceng3505/lingoapp/fragments/FlashcardsFragment.java
+++ app/src/main/java/com/msku/ceng3505/lingoapp/fragments/FlashcardsFragment.java
@@ -25,39 +25,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link FlashcardsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class FlashcardsFragment extends Fragment {
 
     private RecyclerView rvVocabs;
     private VocabularyAdapter adapter;
     private List<Vocabulary> vocabs;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
     public FlashcardsFragment() {
-        // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FlashcardsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
+
     public static FlashcardsFragment newInstance(String param1, String param2) {
         FlashcardsFragment fragment = new FlashcardsFragment();
         Bundle args = new Bundle();
@@ -79,7 +62,6 @@ public class FlashcardsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_flashcards, container, false);
     }
 
@@ -111,17 +93,15 @@ public class FlashcardsFragment extends Fragment {
             String turkish = etTurkish.getText().toString().trim();
 
             if (!english.isEmpty() && !turkish.isEmpty()) {
-                // Listeye ekle
+
                 Vocabulary newVocab = new Vocabulary(english, turkish);
                 vocabs.add(newVocab);
 
-                // Adapter'a bildir
                 adapter.notifyDataSetChanged();
 
                 dialog.dismiss();
             }
         });
-
         dialog.show();
     }
 }
