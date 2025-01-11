a3b7c48b7fd544122cc131bf54369c932b0ff53a
myeniadim <m.yeniadimm@gmail.com>
2025-01-11 18:32:29 +0300

    implement firestore and small screen changes

--- app/src/main/java/com/msku/ceng3505/lingoapp/activities/LoginActivity.java
+++ app/src/main/java/com/msku/ceng3505/lingoapp/activities/LoginActivity.java
@@ -33,17 +33,7 @@ public class LoginActivity extends AppCompatActivity {
     ProgressBar progressBar;
     TextView textView;
 
-    @Override
-    public void onStart() {
-        super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        if(currentUser != null){
-            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-            startActivity(intent);
-            finish();
-        }
-    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/msku/ceng3505/lingoapp/activities/RegisterActivity.java
+++ app/src/main/java/com/msku/ceng3505/lingoapp/activities/RegisterActivity.java
@@ -19,29 +19,26 @@ import androidx.core.view.WindowInsetsCompat;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.Firebase;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.SetOptions;
 import com.msku.ceng3505.lingoapp.R;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RegisterActivity extends AppCompatActivity {
 
-    TextInputEditText etEmail, etPassword;
+    TextInputEditText etEmail, etPassword, etFname, etLname, etUsername;
     Button regBtn;
     FirebaseAuth mAuth;
     ProgressBar progressBar;
+    FirebaseFirestore db;
 
-    @Override
-    public void onStart() {
-        super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        if(currentUser != null){
-            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-            startActivity(intent);
-            finish();
-        }
-    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,9 +46,13 @@ public class RegisterActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_register);
         mAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
 
         etEmail = findViewById(R.id.email);
         etPassword = findViewById(R.id.password);
+        etFname = findViewById(R.id.fName);
+        etLname = findViewById(R.id.lName);
+        etUsername = findViewById(R.id.username);
 
         regBtn = findViewById(R.id.btnRegister);
 
@@ -63,9 +64,12 @@ public class RegisterActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 progressBar.setVisibility(View.VISIBLE);
-                String email, password;
+                String email, password, fName, lName, userName;
                 email = String.valueOf(etEmail.getText());
                 password = String.valueOf(etPassword.getText());
+                fName = String.valueOf(etFname.getText());
+                lName = String.valueOf(etLname.getText());
+                userName = String.valueOf(etUsername.getText());
 
                 if (TextUtils.isEmpty(email)){
                     Toast.makeText(RegisterActivity.this, "Please enter a email", Toast.LENGTH_SHORT).show();
@@ -77,17 +81,51 @@ public class RegisterActivity extends AppCompatActivity {
                     return;
                 }
 
+                if (TextUtils.isEmpty(fName)){
+                    Toast.makeText(RegisterActivity.this, "Please enter a first name", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(lName)){
+                    Toast.makeText(RegisterActivity.this, "Please enter a last name", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(userName)){
+                    Toast.makeText(RegisterActivity.this, "Please enter a username", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
                 mAuth.createUserWithEmailAndPassword(email, password)
                         .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
                                 progressBar.setVisibility(View.GONE);
                                 if (task.isSuccessful()) {
-                                    Toast.makeText(RegisterActivity.this, "Account Created.",
-                                            Toast.LENGTH_SHORT).show();
-                                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                                    startActivity(intent);
-                                    finish();
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    if (user != null) {
+                                        // Save additional user info to Firestore
+
+                                        String uid = user.getUid();
+                                        Map<String, Object> userInfo = new HashMap<>();
+                                        userInfo.put("firstName", fName);
+                                        userInfo.put("lastName", lName);
+                                        userInfo.put("username", userName);
+                                        userInfo.put("email", email);
+
+                                        db.collection("users").document(uid)
+                                                .set(userInfo, SetOptions.merge())
+                                                .addOnCompleteListener(task1 -> {
+                                                    if (task1.isSuccessful()) {
+                                                        Toast.makeText(RegisterActivity.this, "Account Created.", Toast.LENGTH_SHORT).show();
+                                                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                                                        startActivity(intent);
+                                                        finish();
+                                                    } else {
+                                                        Toast.makeText(RegisterActivity.this, "Failed to save user data.", Toast.LENGTH_SHORT).show();
+                                                    }
+                                                });
+                                    }
                                 } else {
                                     // If sign in fails, display a message to the user.
                                     Toast.makeText(RegisterActivity.this, "Authentication failed.",
