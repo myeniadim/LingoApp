9fe3638cb992ff979a1bfecca1e4cfc8a104018f
myeniadim <m.yeniadimm@gmail.com>
2025-01-11 20:42:53 +0300

    change design of flashcards feature

--- app/src/main/java/com/msku/ceng3505/lingoapp/activities/LoginActivity.java
+++ app/src/main/java/com/msku/ceng3505/lingoapp/activities/LoginActivity.java
@@ -34,6 +34,16 @@ public class LoginActivity extends AppCompatActivity {
     TextView textView;
 
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/msku/ceng3505/lingoapp/activities/RegisterActivity.java
+++ app/src/main/java/com/msku/ceng3505/lingoapp/activities/RegisterActivity.java
@@ -39,6 +39,16 @@ public class RegisterActivity extends AppCompatActivity {
     FirebaseFirestore db;
 
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
